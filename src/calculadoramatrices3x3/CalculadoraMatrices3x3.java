0f4bb59ae66f2c2e8dd91e890b857c0b5fa3cace
J <40646732+J020203@users.noreply.github.com>
2021-07-13 20:43:14 -0500

    Add files via upload

---
+++ src/calculadoramatrices3x3/CalculadoraMatrices3x3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoramatrices3x3;
+
+/**
+ *
+ * @author juanc
+ */
+public class CalculadoraMatrices3x3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+InterfaceCalc i = new InterfaceCalc();
+i.setLocationRelativeTo(null);
+i.setVisible(true);
+    }
+}
